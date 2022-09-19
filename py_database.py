218ab99fec3224a0351b20df635ec9ffb473007e
Jules Ezeh <julesezeh23@gmail.com>
2022-09-20 00:12:31 +0100

    before pasting from source code

---
+++ py_database.py
@@ -0,0 +1,27 @@
+#import sqlite3
+import sqlite3
+
+#establish a connection to the database 
+conn = sqlite3.connect("students.db")
+
+#create a cursor to perform actions 
+cur = conn.cursor()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+
+cur.execute("""
+	CREATE TABLE students (
+		name TEXT,
+		age INTEGER,
+		height REAL,
+	) """)
+
+cur.execute("INSERT INTO students VALUES  ('Marty',42,1.8)")
+
+all_students = [
+	('John',24,1.78),
+	("Lupita",21,2.01),
+	("Kevin",18,2.05),
+]
+cur.executemany("INSERT INTO students VALUES (?,?,?)",all_students)
+
+conn.commit()
+conn.close()    
